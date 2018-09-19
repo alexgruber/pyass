bc7a6d41405fe125c203ea11a6a72e9d12b0de44
Alexander Gruber <reddy.gruber@icloud.com>
2018-09-19 09:44:02 +0200

    updated KF and 2D KF

--- filter.py
+++ filter.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from numpy.linalg import inv
-from numpy import transpose, dot
+from numpy import dot
 
 from copy import deepcopy
 
@@ -19,20 +19,82 @@ def analyse(x, y, P, R, H=None):
 
     return x_upd, P_upd, K
 
+
+def analyse_2d(x, y, P, R, H=None):
+
+    x, y = np.atleast_2d(x), np.atleast_2d(y)
+    P, R = np.atleast_2d(P), np.atleast_2d(R)
+
+    if H is None:
+        H = np.atleast_2d(np.ones(len(x))).T
+
+    H = H.reshape((H.size,1))
+    x = x.reshape((x.size,1))
+    y = y.reshape((y.size,1))
+
+    R = np.identity(R.size) * R
+
+    K = dot(P, dot(H.T, inv(R + dot(H, dot(P, H.T)))))
+    x_upd = x + dot(K, y - dot(H, x))
+    P_upd = dot(np.identity(P.shape[0]) - dot(K, H), P)
+
+    return x_upd, P_upd, K.flatten()
+
 def KF(model, forcing, obs, R, H=None):
 
-    x_ana, P_ana = np.full(len(forcing), np.nan), np.full(len(forcing), np.nan)
+    dummy = np.full(len(forcing), np.nan)
+    x_ana, P_ana, norm_innov, K_arr = dummy.copy(), dummy.copy(), dummy.copy(), dummy.copy()
 
     for t, f in enumerate(forcing):
         x, P = model.step(f)
-
         y = obs[t]
-        x_upd, P_upd = analyse(x, y, P, R, H=H)
 
-        x_ana[t], P_ana[t] = x_upd, P_upd
-        model.x, model.P = x_upd, P_upd
+        if not np.isnan(y):
+            norm_innov[t] = (H*y - x) / np.sqrt(P + H**2 * R)
 
-    return x_ana, P_ana
+            x_ana[t], P_ana[t], K_arr[t] = analyse(x, y, P, R, H=H)
+            model.x, model.P = x_ana[t], P_ana[t]
+        else:
+            x_ana[t], P_ana[t] = x, P
+
+    K = np.nanmean(K_arr)
+    check_var = np.nanvar(norm_innov,ddof=1)
+
+    return x_ana, P_ana, check_var, K
+
+def KF_2D(model, forcing, obs1, obs2, R, H=None):
+
+    dummy = np.full(len(forcing), np.nan)
+    x_ana, P_ana, K1, K2 = dummy.copy(), dummy.copy(), dummy.copy(), dummy.copy()
+    norm_innov1, norm_innov2, norm_innov3 = dummy.copy(), dummy.copy(), dummy.copy()
+
+    for t, f in enumerate(forcing):
+        x, P = model.step(f)
+        y = np.array([obs1[t], obs2[t]])
+
+        if not np.isnan(y[0]):
+            norm_innov1[t] = (y[0]/H[0] - x) / np.sqrt(P + R[0]/H[0]**2)
+        if not np.isnan(y[1]):
+            norm_innov2[t] = (y[1]/H[1] - x) / np.sqrt(P + R[1]/H[1]**2)
+
+        if len(np.where(np.isnan(y))[0]) == 0:
+            norm_innov3[t] = (2*x - y[0]/H[0] - y[1]/H[1]) / np.sqrt(4*P + R[0]/H[0]**2 + R[1]/H[1]**2)
+
+        tmp_R = R.copy()
+        ind_nan = np.where(np.isnan(y))
+        y[ind_nan] = 0.
+        tmp_R[ind_nan] = 999999.
+        x_ana[t], P_ana[t], (K1[t], K2[t]) = analyse_2d(x, y, P, tmp_R, H=H)
+        model.x, model.P = x_ana[t], P_ana[t]
+
+    norm_innov1 = np.nanvar(norm_innov1,ddof=1)
+    norm_innov2 = np.nanvar(norm_innov2,ddof=1)
+    norm_innov3 = np.nanvar(norm_innov3,ddof=1)
+
+    K1 = np.nanmean(K1)
+    K2 = np.nanmean(K2)
+
+    return x_ana, P_ana, norm_innov1, norm_innov2, norm_innov3, K1, K2
 
 def generate_ensemble(data, n_ens, params):
 
@@ -82,7 +144,7 @@ def EnKF(model, forcing, obs, force_pert, obs_pert, H=None, n_ens=24):
             P = x_ens.var(ddof=1)
             R = y_ens.var(ddof=1)
 
-            norm_innov[t] = ((H*y_ens).mean() - x_ens.mean()) / np.sqrt(P + H**2*R)
+            norm_innov[t] = (H*(y_ens.mean()) - x_ens.mean()) / np.sqrt(P + H**2*R)
 
             # update state of each ensemble member
             x_ens_upd = np.full(n_ens, np.nan)
